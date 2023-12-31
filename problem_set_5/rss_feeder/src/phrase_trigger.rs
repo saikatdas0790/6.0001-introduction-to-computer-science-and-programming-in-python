a3a18e0d0c28fc15092627d38399404fb2cac1d0
Saikat Das <saikatdas0790@gmail.com>
2023-12-31 12:48:54 +0530

    feat: Add PhraseTriggerImpl struct and implementation

--- problem_set_5/rss_feeder/src/phrase_trigger.rs
+++ problem_set_5/rss_feeder/src/phrase_trigger.rs
@@ -4,3 +4,19 @@ pub trait PhraseTrigger: Trigger {
     fn new(phrase: &str) -> Self;
     fn is_phrase_in(&self, text: &str) -> bool;
 }
+
+pub struct PhraseTriggerImpl {
+    phrase: String,
+}
+
+impl PhraseTriggerImpl {
+    pub fn new(phrase: &str) -> Self {
+        PhraseTriggerImpl {
+            phrase: phrase.to_ascii_lowercase(),
+        }
+    }
+
+    pub fn is_phrase_in(&self, text: &str) -> bool {
+        text.to_ascii_lowercase().contains(&self.phrase)
+    }
+}
